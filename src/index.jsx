21ba71f21b1755db8af77d4a134deccb611e7073
dharani277 <60962689+dharani277@users.noreply.github.com>
2020-07-24 12:08:08 +0530

    updated

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Login from "./Router/Login";
+import App from "./Router/App";
 import * as serviceWorker from "./serviceWorker";
 ReactDOM.render(
   <React.StrictMode>
-    <Login />
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
